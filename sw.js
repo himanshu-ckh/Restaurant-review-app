ccd7d42257abeb19d2d0fa8e4fef03f5a8e92236
Himanshu Chaudhary <himanshu.c.k.h@gmail.com>
2018-05-10 12:16:10 +0530

    Updated the urls to cache

--- sw.js
+++ sw.js
@@ -7,24 +7,23 @@ const CACHENAME = 'restaurant-cache';
 *All the local urls which we want to add to the cache
 */
 const CACHENAME_URLS = [
-  './',
-  '/index.html',
-  '/restaurant.html',
-  '/css/styles.css',
-  '/js/dbhelper.js',
-  '/js/main.js',
-  '/js/restaurant_info.js',
-  '/data/restaurants.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/5.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg'
+'/index.html',
+'/restaurant.html',
+'/css/styles.css',
+'/js/dbhelper.js',
+'/js/main.js',
+'/js/restaurant_info.js',
+'/data/restaurants.json',
+'/img/1.jpg',
+'/img/2.jpg',
+'/img/3.jpg',
+'/img/4.jpg',
+'/img/5.jpg',
+'/img/6.jpg',
+'/img/7.jpg',
+'/img/8.jpg',
+'/img/9.jpg',
+'/img/10.jpg'
 ];
 
 /**
