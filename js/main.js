dbf3201ab42b2a64bbb6eb4eea0dc906be3e4aa0
Himanshu Chaudhary <himanshu.c.k.h@gmail.com>
2018-05-11 14:50:37 +0530

    Lower smenatic

--- js/main.js
+++ js/main.js
@@ -151,7 +151,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute("alt", restaurant.alt);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
